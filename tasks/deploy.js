8bc23d44391e2c929e5813c60e811ddcc5479592
Grigory-F <grishka2500@gmail.com>
2022-12-26 13:01:45 +0400

    awdadaw

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -8,7 +8,7 @@ const connect = ftp.create(ftpSettings);
 
 module.exports = function deploy() {
 	return src(['build/**/*.*', '!build/**/*.map'])
-		.pipe(connect.newer('public_html/'))
-		.pipe(connect.dest('public_html/'))
+		.pipe(connect.newer('booksLanding/public_html/'))
+		.pipe(connect.dest('booksLanding/public_html/'))
 		.on('end', () => console.log(`Finished deploing ./build to https://${chalk.blueBright(ftpSettings.host)}`))
 }
